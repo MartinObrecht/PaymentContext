b9a1c7ac10ada6b45a600015087556969a0cff9b
Martin Obrecht <martin.obrecht@yahoo.com.br>
2021-09-25 08:01:36 -0300

    Add project files.

---
+++ PaymentContext.Domain/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentContext.Domain
+{
+    class Student
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
